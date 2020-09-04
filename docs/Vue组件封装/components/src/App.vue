dc8f6715ab9647750c0216c025fd728ec39a27bc
sanSkiller <1290148953@qq.com>
2020-09-04 09:47:09 +0800

    updated:添加不同种类按钮组件

--- docs/Vue组件封装/components/src/App.vue
+++ docs/Vue组件封装/components/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <c-button>11</c-button>
-    <c-button>22</c-button>
-    <c-button>33</c-button>
+    <c-button type="primary">22</c-button>
+    <c-button type="success">33</c-button>
+    <c-button type="info">44</c-button>
+    <c-button type="warning">55</c-button>
+    <c-button type="danger">66</c-button>
   </div>
 </template>
 
--- docs/Vue组件封装/components/src/components/Button.vue
+++ docs/Vue组件封装/components/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="c-button">
+  <button class="c-button" :class="[`c-button--${type}`]">
     <span>
       <slot></slot>
     </span>
@@ -9,6 +9,12 @@
 <script>
 export default {
   name: "CButton",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+  },
 };
 </script>
 
@@ -38,6 +44,71 @@ export default {
     color: #409eff;
     border-color: #c6e2ff;
     background-color: #ecf5ff;
+  }
+}
+
+.c-button--primary {
+  color: #fff;
+  background-color: #409eff;
+  border-color: #409eff;
+
+  &:hover,
+  &:focus {
+    background: #66b1ff;
+    border-color: #66b1ff;
+    color: #fff;
+  }
+}
+
+.c-button--success {
+  color: #fff;
+  background-color: #67c23a;
+  border-color: #67c23a;
+
+  &:hover,
+  &:focus {
+    background: #85ce61;
+    border-color: #85ce61;
+    color: #fff;
+  }
+}
+
+.c-button--info {
+  color: #fff;
+  background-color: #909399;
+  border-color: #909399;
+
+  &:hover,
+  &:focus {
+    background: #a6a9ad;
+    border-color: #a6a9ad;
+    color: #fff;
+  }
+}
+
+.c-button--warning {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+
+  &:hover,
+  &:focus {
+    background: #ebb563;
+    border-color: #ebb563;
+    color: #fff;
+  }
+}
+
+.c-button--danger {
+  color: #fff;
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+
+  &:hover,
+  &:focus {
+    background: #f78989;
+    border-color: #f78989;
+    color: #fff;
   }
 }
 </style>
